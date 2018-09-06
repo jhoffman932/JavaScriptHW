45e62d8edee055cd0feba85a4f67b01f5eab744f
Josh Hoffman <jhoffman2@radford.edugit config --global user.name Josh>
2018-09-06 17:50:51 -0400

    HW

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// Get references to the tbody element, input field and button
+var $tbody = document.querySelector('tbody');
+var $stateInput = document.querySelector('#state');
+var $searchBtn = document.querySelector('#search');
+
+// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
+window.onload=function(){
+  $searchBtn.addEventListener('click', handleSearchButtonClick);
+}
+
+
+// Set filteredData to data initially
+// addressData comes from the data.js file
+var filteredAddresses = data;
+
+// Fill the empty table with data from data
+function renderTable() {
+    $tbody.innerHTML = '';
+    for (var i = 0; i < filteredAddresses.length; i++) {
+      // Get get the current address object and its fields
+      var address = filteredAddresses[i];
+      var fields = Object.keys(address);
+      // Create a new row in the tbody, set the index to be i + startingIndex
+      var $row = $tbody.insertRow(i);
+      for (var j = 0; j < fields.length; j++) {
+        // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+        var field = fields[j];
+        var $cell = $row.insertCell(j);
+        $cell.innerText = address[field];
+      }
+    }
+  }
+
+  function handleSearchButtonClick() {
+    var filterState = $stateInput.value.trim().toLowerCase();
+    filteredAddresses = data.filter(function(address) {
+      var addressState = data.state.toLowerCase();
+      return addressState === filterState;
+    });
+
+    renderTable();
+    };
+
+    function handleResetButtonClick() {
+      filteredAddresses = data;
+      $cityInput.value = '';
+      $stateInput.value = '';
+      renderTable();
+    };
+
+  // Render the table for the first time on page load
+  renderTable();
